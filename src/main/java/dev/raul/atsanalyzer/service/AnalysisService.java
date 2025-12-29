83be2c02411f16c2dd1f19e3f704390227ab618c
raulmaciel <raulmacielxd@gmail.com>
2025-12-29 18:37:23 -0300

    Developed text normalization converting to lowercase, removing pontuations, accents and normalizing multiple spaces

--- src/main/java/dev/raul/atsanalyzer/service/AnalysisService.java
+++ src/main/java/dev/raul/atsanalyzer/service/AnalysisService.java
@@ -4,21 +4,28 @@ import dev.raul.atsanalyzer.dto.request.JobDescriptionDTO;
 import dev.raul.atsanalyzer.dto.response.SendJobDescriptionResponseDTO;
 import dev.raul.atsanalyzer.repository.AnalysisRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor(onConstructor_ = @__(@Autowired)) @Slf4j
 public class AnalysisService {
     private final AnalysisRepository analysisRepository;
+    private final TextNormalizationService textNormalizationService;
 
-    @Autowired
-    public AnalysisService(AnalysisRepository analysisRepository) {
-        this.analysisRepository = analysisRepository;
-    }
 
-    @Transactional
     public SendJobDescriptionResponseDTO createJobAnalysis(JobDescriptionDTO request){
+        List<String> minimumNormalization = textNormalizationService.minimumNormalization(request.descriptions());
+        for (String text : minimumNormalization){
+            log.info(text);
+        }
+
+
         return SendJobDescriptionResponseDTO.builder()
                 .descriptionCount(request.descriptions().size())
                 .build();
