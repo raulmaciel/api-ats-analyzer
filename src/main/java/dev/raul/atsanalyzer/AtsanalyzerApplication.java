b3077e6c4d689af5c27099cdc8edd1fad17edd0c
raulmaciel <raulmacielxd@gmail.com>
2025-12-29 13:30:33 -0300

    first commit

---
+++ src/main/java/dev/raul/atsanalyzer/AtsanalyzerApplication.java
@@ -0,0 +1,13 @@
+package dev.raul.atsanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/raul/atsanalyzer/AtsanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raul.atsanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
