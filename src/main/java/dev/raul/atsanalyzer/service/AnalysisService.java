f90fc6a824ed9e7bbd8190fd95015a867e55e5cf
raulmaciel <raulmacielxd@gmail.com>
2025-12-30 14:15:53 -0300

    Developed semantic normalization, that substitute equivalents terms to a unique term

--- src/main/java/dev/raul/atsanalyzer/service/AnalysisService.java
+++ src/main/java/dev/raul/atsanalyzer/service/AnalysisService.java
@@ -3,11 +3,9 @@ package dev.raul.atsanalyzer.service;
 import dev.raul.atsanalyzer.dto.request.JobDescriptionDTO;
 import dev.raul.atsanalyzer.dto.response.SendJobDescriptionResponseDTO;
 import dev.raul.atsanalyzer.repository.AnalysisRepository;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,13 +14,13 @@ import java.util.List;
 @AllArgsConstructor(onConstructor_ = @__(@Autowired)) @Slf4j
 public class AnalysisService {
     private final AnalysisRepository analysisRepository;
-    private final TextNormalizationService textNormalizationService;
+    private final NormalizationService textNormalizationService;
 
 
     public SendJobDescriptionResponseDTO createJobAnalysis(JobDescriptionDTO request){
-        List<String> minimumNormalization = textNormalizationService.minimumNormalization(request.descriptions());
-        for (String text : minimumNormalization){
-            log.info(text);
+        List<String> fullNormalization = textNormalizationService.fullNormalization(request.descriptions());
+        for (String text : fullNormalization){
+            log.debug(text);
         }
 
 
---
+++ src/main/java/dev/raul/atsanalyzer/service/NormalizationService.java
@@ -0,0 +1,61 @@
+package dev.raul.atsanalyzer.service;
+
+import org.springframework.stereotype.Service;
+
+import java.text.Normalizer;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+
+@Service
+public class NormalizationService {
+    private static final Map<String, String> SEMANTIC_MAP = Map.of(
+            "spring-boot","springboot",
+            "microservicos","microservices",
+            "micro servicos","microservices",
+            "micro-serviços","microservices",
+            "microsservicos","microservices",
+            "restful","rest",
+            "rest api","api rest"
+    );
+
+    public List<String> fullNormalization(List<String> descriptions) {
+        if (descriptions == null) return List.of();
+
+        return descriptions.stream()
+                .map(NormalizationService::basicTextNormalizer)
+                .map(this::semanticNormalizer)
+                .collect(Collectors.toList());
+    }
+
+
+    private String semanticNormalizer(String text){
+        String result = text;
+
+        for (Map.Entry<String, String> entry : SEMANTIC_MAP.entrySet()){
+            String regex = "\\b" + Pattern.quote(entry.getKey()) + "\\b";
+            result = result.replaceAll(regex, entry.getValue());
+        }
+
+        return result.replaceAll("\\s+", " ").trim();
+    }
+
+
+    private static String basicTextNormalizer(String text) {
+        if (text == null) return "";
+
+        String t = text.toLowerCase();
+        t = t.replace("c#", "csharp");
+        t = t.replace(".net", "dotnet");
+
+        String normalizeText = Normalizer.normalize(t, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
+                .replaceAll("\\p{Punct}", " ")
+                .replaceAll("\\s+", " ").trim();
+
+
+        return normalizeText;
+    }
+}
