3af84ef180168e2b99974f46a56e2b8fd41cfed5
raulmaciel <raulmacielxd@gmail.com>
2025-12-30 18:12:56 -0300

    add tests to tokenization service

--- src/test/java/dev/raul/atsanalyzer/service/TokenizationServiceTest.java
+++ src/test/java/dev/raul/atsanalyzer/service/TokenizationServiceTest.java
@@ -1,4 +1,33 @@
+package dev.raul.atsanalyzer.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TokenizationServiceTest {
-  
+
+    @Test
+    @DisplayName("Should split each text into tokens by whitespace")
+    void tokenizeTexts() {
+        List<String> input = List.of("java  spring boot",
+                "JAVA e frameworks Spring",
+                "Testes unitários integração");
+
+        NormalizationService normalizationService = new NormalizationService();
+        List<String> nomalizedInput = normalizationService.fullNormalization(input);
+
+        TokenizationService tokenizationService = new TokenizationService();
+        List<List<String>> tokenizedTexts = tokenizationService.tokenizeTexts(nomalizedInput);
+
+        assertEquals(3, tokenizedTexts.size());
+
+        assertEquals(List.of("java", "springboot"), tokenizedTexts.get(0));
+        assertEquals(List.of("java", "e", "frameworks", "spring"), tokenizedTexts.get(1));
+        assertEquals(List.of("testes", "unitarios", "integracao"), tokenizedTexts.get(2));
+
+    }
 }
